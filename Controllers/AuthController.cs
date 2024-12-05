19fb30a5bbde6c9d12bb129ad78a5e1f777f0fa1
vmujic1 <vmujic1@etf.unsa.ba>
2024-12-05 21:21:41 +0100

    Implemented login route

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using SymphonyEquilibriAPI.Dtos.User;
 using SymphonyEquilibriAPI.Models;
 using SymphonyEquilibriAPI.Services.AuthService;
@@ -15,13 +16,26 @@ namespace SymphonyEquilibriAPI.Controllers
         {
             this._authService = authService;
         }
-
         [HttpPost("register")]
         public async Task<ActionResult<ServiceResponse<int>>> Register(UserRegisterDto request)
         {
             var response = await _authService.Register(
                 new Models.User.User { Username = request.Username }, request.Password
             );
+
+            if (!response.Success)
+            {
+                return BadRequest(response);
+            }
+
+            return Ok(response);
+        }
+
+
+        [HttpPost("login")]
+        public async Task<ActionResult<ServiceResponse<string>>> Login(UserLoginDto request)
+        {
+            var response = await _authService.Login(request.Username, request.Password);
 
             if (!response.Success)
             {
--- Services/AuthService/AuthService.cs
+++ Services/AuthService/AuthService.cs
@@ -13,9 +13,28 @@ namespace SymphonyEquilibriAPI.Services.AuthService
         {
             this._context = context;
         }
-        public Task<ServiceResponse<string>> Login(string username, string password)
+        public async Task<ServiceResponse<string>> Login(string username, string password)
         {
-            throw new NotImplementedException();
+            var serviceResponse = new ServiceResponse<string>();
+            var user = await _context.Users
+                .FirstOrDefaultAsync(u => u.Username.ToLower().Equals(username.ToLower()));
+            if (user == null)
+            {
+                serviceResponse.Success = false;
+                serviceResponse.Message = "User not found!";
+            }
+            else if(!VerifyPasswordHash(password, user.PasswrodHash, user.PasswordSalt))
+            {
+                serviceResponse.Success = false;
+                serviceResponse.Message = "Wrong password.";
+            } 
+            else
+            {
+                serviceResponse.Data = user.Id.ToString();
+                serviceResponse.Message = "Successfully logged in!";
+            }
+
+            return serviceResponse;
         }
 
         public async Task<ServiceResponse<int>> Register(User user, string password)
@@ -57,6 +76,16 @@ namespace SymphonyEquilibriAPI.Services.AuthService
                 passwrodSalt = hmac.Key;
                 passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
             }
+        }
+
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt))
+            {
+                var computeHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                return computeHash.SequenceEqual(passwordHash);
+            }
+
         }
     }
 }
